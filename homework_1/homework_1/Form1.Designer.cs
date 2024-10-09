3cb6d6fc382548e0116739ef8be2bc4fd8d3a2bd
Viiiiin <capriotti.1984129@studenti.uniroma1.it>
2024-10-09 23:38:30 +0200

    Aggiunto hm1

---
+++ homework_1/homework_1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace homework_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtserver = new System.Windows.Forms.TextBox();
+            this.txtattacker = new System.Windows.Forms.TextBox();
+            this.txtprobability = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtserver
+            // 
+            this.txtserver.Location = new System.Drawing.Point(107, 9);
+            this.txtserver.Name = "txtserver";
+            this.txtserver.Size = new System.Drawing.Size(100, 20);
+            this.txtserver.TabIndex = 0;
+            this.txtserver.TextChanged += new System.EventHandler(this.txtserver_TextChanged);
+            // 
+            // txtattacker
+            // 
+            this.txtattacker.Location = new System.Drawing.Point(335, 9);
+            this.txtattacker.Name = "txtattacker";
+            this.txtattacker.Size = new System.Drawing.Size(100, 20);
+            this.txtattacker.TabIndex = 1;
+            this.txtattacker.TextChanged += new System.EventHandler(this.txtattacker_TextChanged);
+            // 
+            // txtprobability
+            // 
+            this.txtprobability.Location = new System.Drawing.Point(541, 9);
+            this.txtprobability.Name = "txtprobability";
+            this.txtprobability.Size = new System.Drawing.Size(100, 20);
+            this.txtprobability.TabIndex = 2;
+            this.txtprobability.TextChanged += new System.EventHandler(this.txtprobability_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Attacker number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(458, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Probability";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 42);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "CLOSE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1153, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtprobability);
+            this.Controls.Add(this.txtattacker);
+            this.Controls.Add(this.txtserver);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtserver;
+        private System.Windows.Forms.TextBox txtattacker;
+        private System.Windows.Forms.TextBox txtprobability;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ homework_1/homework_1/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace homework_1
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        private int server;
+        private int attacker;
+        private float probability;
+        private int[] result;
+        private float x_space;
+        private float y_space;
+        private int height;
+        private int width;
+        private const int START_X = 40;
+        private const int START_Y = 100;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+
+
+            this.random = new Random();
+            this.height = this.ClientSize.Height - 150;
+            this.width = (this.ClientSize.Width/2) - 50;
+            this.Paint += new PaintEventHandler(Create_Graphic);
+            this.Paint += new PaintEventHandler(Paint_Attack);
+            this.Paint += new PaintEventHandler(Create_Graphic_result);
+            this.Paint += new PaintEventHandler(Paint_Result);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int attacker, server;
+            float probability;
+            bool parseServer = int.TryParse(txtserver.Text, out server);
+            bool parseAtk = int.TryParse(txtattacker.Text, out attacker);
+            bool parseProb = float.TryParse(txtprobability.Text, out probability);
+            if (parseServer && parseAtk && parseProb)
+            {
+                this.server = server;
+                this.attacker = attacker;
+                this.probability = probability;
+                this.result = new int[server + 1];
+                this.x_space = this.width / (float)this.server;
+                this.y_space = this.height / (float)this.server;
+                this.Invalidate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void checkvalue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void checkProbability_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            System.Windows.Forms.TextBox textbox = sender as System.Windows.Forms.TextBox;
+
+            if (textbox.Text == "" && e.KeyChar != '0' && e.KeyChar != '1' && e.KeyChar != '\\')
+            {
+                e.Handled = true;
+            }
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != '\\')
+            {
+                e.Handled = true;
+            }
+            if (textbox.Text == "0" && e.KeyChar != '.' && e.KeyChar != '\\')
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == '.' && textbox.Text.Contains(".") && e.KeyChar != '\\')
+            {
+                e.Handled = true;
+            }
+
+        }
+
+       
+
+        
+
+
+    private void Paint_Attack(object sender, PaintEventArgs e)
+    {
+        if (this.attacker ==0 || this.server == 0)
+            {
+                return;
+            }
+        Graphics g = e.Graphics;
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+        int count;
+        Color color;
+        PointF current_point, next_point;
+        Pen pen;
+        for (int i = 0; i < this.attacker; i++)
+        {
+            count = 0;
+            color = Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+            current_point = new PointF(START_X, this.height+START_Y);
+            next_point = new PointF(START_X, this.height+START_Y);
+            pen = new Pen(color, 3);
+
+            for (int j = 0; j < this.server; j++)
+            {
+                if (Attack())
+                {
+                    next_point = new PointF(current_point.X, current_point.Y - y_space);
+                    g.DrawLine(pen, current_point, next_point);
+                    current_point = next_point;
+                    count++;
+                }
+
+                next_point = new PointF(current_point.X + x_space, current_point.Y);
+                g.DrawLine(pen, current_point, next_point);
+                current_point = next_point;
+            }
+
+            this.result[count] += 1;
+        }
+    }
+
+    private Boolean Attack()
+    {
+        float randomNumber = (float)random.NextDouble();
+        return this.probability >= randomNumber;
+    }
+
+    private void Create_Graphic(object sender, PaintEventArgs e)
+    {
+        if (this.attacker == 0 || this.server == 0)
+        {
+            return;
+        }
+        Graphics g = e.Graphics;
+
+        Pen blackPen = new Pen(Color.Black, 2);
+        Rectangle rect = new Rectangle(START_X, START_Y, this.width, this.height);
+        g.DrawRectangle(blackPen, rect);
+    }
+
+        private void Paint_Result(object sender, PaintEventArgs e)
+        {
+            if (this.attacker == 0 || this.server == 0)
+            {
+                return;
+            }
+            float space = ((float)this.width / 2) / this.attacker;
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black, 1);
+            PointF current_point, next_point;
+            current_point = new PointF(START_X+ this.width, this.height + START_Y);
+            Font font = new Font("Arial", 12);
+
+            for (int i = 0; i < this.result.Length; i++)
+            {
+
+                if (this.result[i] > 0)
+                {
+                    next_point = new PointF(current_point.X + (this.result[i] * space), current_point.Y );
+                    RectangleF rect = new RectangleF(current_point.X, current_point.Y, (this.result[i] * space),this.y_space);
+                    g.FillRectangle(Brushes.Blue, rect); 
+
+                    g.DrawRectangle(pen, Rectangle.Round(rect)); 
+                }
+                current_point.Y -= this.y_space;
+            }
+            float avg = average(0,0,0);
+            g.DrawString($" The average is: {avg}", font, Brushes.Black, 700, 10);
+
+        }
+        private void Create_Graphic_result(object sender, PaintEventArgs e)
+        {
+            if (this.attacker == 0 || this.server == 0)
+            {
+                return;
+            }
+            Graphics g = e.Graphics;
+            float space = ((float)this.width / 2) / this.attacker;
+            Pen blackPen = new Pen(Color.Black, 2);
+            blackPen.DashStyle = DashStyle.Dash;
+
+            Rectangle rect = new Rectangle(this.width + START_X, START_Y, this.width / 2,this.height);
+            g.DrawRectangle(blackPen, rect);
+            Font font = new Font("Arial", 5);
+            int len = this.result.Length;
+            Pen grayPen = new Pen(Color.Gray, 1);
+            float labelOffsetX = (this.width/2) / len;
+            float labelOffsetY = (this.height) / this.attacker;
+            float yPosition = this.height + START_Y;
+            for (int i = 0; i <= this.attacker; i++)
+            {
+                float xPosition = this.width + START_X + (i * space);
+                g.DrawLine(grayPen, xPosition, yPosition, xPosition, yPosition + 10);
+                if (i % labelOffsetY == 0)
+                {
+
+                    g.DrawString($"{i}", font, Brushes.Black, xPosition, yPosition + 12);
+
+                }
+
+            }
+
+           
+        }
+
+        private float average(int i, float cumulativeAverage, int numberOfAttempts)
+        {
+            // Caso base: se abbiamo superato la lunghezza dell'array, ritorna la media finale
+            if (i >= result.Length)
+            {
+                return numberOfAttempts > 0 ? cumulativeAverage : 0;
+            }
+
+            // Verifica se il valore attuale è maggiore di zero
+            if (result[i] > 0)
+            {
+                numberOfAttempts++; // Incrementa il conteggio dei tentativi
+                cumulativeAverage += (result[i] - cumulativeAverage) / numberOfAttempts; // Aggiorna la media cumulativa
+            }
+
+            // Chiamata ricorsiva per il prossimo indice
+            return average(i + 1, cumulativeAverage, numberOfAttempts);
+        }
+
+        private void txtserver_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtattacker_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtprobability_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
